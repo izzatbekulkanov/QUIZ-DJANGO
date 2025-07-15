4d9c0966fcd42388f7d3f0b7a891b34c2600d78e
Izzatbek Ulkanov <izzatbekulkanov@gmail.com>
2025-07-15 12:16:00 +0500

    aa

--- core/settings.py
+++ core/settings.py
@@ -122,10 +122,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_db',
+        'USER': 'test',
+        'PASSWORD': 'test1231',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
